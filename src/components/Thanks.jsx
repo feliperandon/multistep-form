00aa3a752b1661b73f0f53201bde765e8c318404
feliperandon <feliperandon32@gmail.com>
2024-04-21 17:46:24 -0300

    Thanks component and style

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -9,8 +9,20 @@ import "./Thanks.css";
 
 const Thanks = () => {
   return (
-    <div>
-      <h2>Thanks</h2>
+    <div className="thanks-container">
+      <h2>Almost there</h2>
+      <p>
+        Your opinion is very important to us. You will receive a 10% discount
+        cupom for your next purchase.
+      </p>
+      <p>To finish your review, click on Submit button down below.</p>
+      <h3>Here is your review:</h3>
+      <p className="review-data">
+        <span>Satisfiaction:</span>
+      </p>
+      <p className="review-data">
+        <span>Comment:</span>
+      </p>
     </div>
   );
 };
