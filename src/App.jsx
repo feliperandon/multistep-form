495b509562fa9bf2f767c4c059c043ba000722c1
feliperandon <feliperandon32@gmail.com>
2024-04-21 16:26:14 -0300

    main component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+// Components
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>React Multistep</h1>
+      <div className="header">
+        <h2>Rate us!</h2>
+        <p>
+          We appreciate your purchase, use the form below to rate the product
+        </p>
+      </div>
+      <div className="form-container">
+        <p>Steps</p>
+        <form>
+          <div className="action">
+            <button type="button">
+              <GrFormPrevious />
+              <span>Back</span>
+            </button>
+            <button type="submit">
+              <span>Next</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
