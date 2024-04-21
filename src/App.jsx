3d523c5be652e71b750082a9ba9820eaf2a5e84d
feliperandon <feliperandon32@gmail.com>
2024-04-21 16:37:18 -0300

    first components and custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 // Components
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+
+// Hooks
+import { useForm } from "./hooks/useForm";
 
 import "./App.css";
 
 function App() {
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
+
+  const { currentStep, currentComponent } = useForm(formComponents);
+
   return (
     <div className="app">
       <div className="header">
-        <h2>Rate us!</h2>
+        <h2>Review!</h2>
         <p>
-          We appreciate your purchase, use the form below to rate the product
+          We appreciate your purchase, use the form below to review the product
         </p>
       </div>
       <div className="form-container">
         <p>Steps</p>
         <form>
+          <div className="inputs-container">{currentComponent}</div>
           <div className="action">
             <button type="button">
               <GrFormPrevious />
