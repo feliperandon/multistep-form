27d21e3fb1350bf25d92d467e234a8129cf8aab2
feliperandon <feliperandon32@gmail.com>
2024-04-21 17:03:12 -0300

    initial styling

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -3,7 +3,26 @@ import React from "react";
 const UserForm = () => {
   return (
     <div>
-      <h2>User Form</h2>
+      <div className="form-control">
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Your name"
+          required
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="name">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Your email"
+          required
+        />
+      </div>
     </div>
   );
 };
